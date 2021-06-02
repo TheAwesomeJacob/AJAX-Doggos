8ab28d74d19abb42ecd654c0058728b46838dc91
Jacob <66869348+TheAwesomeJacob@users.noreply.github.com>
2021-06-02 18:48:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
+const button = document.querySelector('.button2')
+
 const promise = fetch(DOG_URL);
 const doggos = document.querySelector('.doggos');
 
+button.addEventListener("click", () =>{
 promise.then(response => {
     const processingPromise = response.json();
     return processingPromise;
+
 }).then(proccessedRespponse => {
+
     const img = document.createElement("img");
     img.src = proccessedRespponse.message;
     img.alt = "Cute doggo";
     doggos.appendChild(img);
+
+})
 })
